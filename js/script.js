a0459a7e121b301efae3f7695e57e0f94824d9e6
Abhishek-Kumar08 <aman12348590@gmail.com>
2022-08-14 02:22:12 +0530

    New Changes

--- js/script.js
+++ js/script.js
@@ -7,3 +7,22 @@ $(function () {
     }
   });
 });
+
+var c = document.getElementById("createDiv");
+var p = document.getElementById("divPass");
+var s = document.getElementById("signDiv");
+var cb = document.getElementById("continue");
+
+function createNew(){
+  s.className = 'hidden';
+  c.className = '';
+}
+function btnContinue(){
+  c.className = 'hidden';
+  p.className = '';
+}
+function signIn(){
+  c.className = 'hidden';
+  p.className = 'hidden';
+  s.className = '';
+}
