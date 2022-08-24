36e59c0f550d42b6d1ef50a6f68b285a76e1c355
Abhishek-Kumar08 <aman12348590@gmail.com>
2022-08-24 07:38:01 +0530

    New Files

--- js/cart.js
+++ js/cart.js
@@ -1,10 +1,15 @@
-document.addEventListener("DOMContentLoaded",(e)=>{
+document.addEventListener("DOMContentLoaded",()=>{
     if(sessionStorage.getItem("item")!=null){
         document.getElementById("cartH").className = 'hidden';  
+        document.getElementById("priceDetails").className = 'container';  
+        document.getElementById("payment").className = 'container';  
+        document.getElementById("totalPrice").className = 'container';  
         document.getElementById("cartContent").innerHTML += sessionStorage.getItem("item");
+        document.getElementById("priceDetails").innerHTML = sessionStorage.getItem("priceDetail");
+        document.getElementById("totalPrice").innerHTML = sessionStorage.getItem("totalPriceDetail");
     }
 });
-document.querySelector("#resetCart").addEventListener("click",()=>{
-    document.getElementById("divCart").innerHTML = "";
+function removeItem(){
+    document.getElementById("cartContent").innerHTML = "";
     sessionStorage.clear();
-});
+}
--- js/menu.js
+++ js/menu.js
@@ -1,62 +1,102 @@
-var vp = document.getElementsByClassName("vPizza");
-var nvp = document.getElementsByClassName("nvPizza");
-var vb = document.getElementsByClassName("vBurger");
-var nvb = document.getElementsByClassName("nvBurger");
-var vf = document.getElementsByClassName("vFrankie");
-var nvf = document.getElementsByClassName("nvFrankie");
-var vpa = document.getElementsByClassName("vPasta");
-var nvpa = document.getElementsByClassName("nvPasta");
-
-function addMe(s,e){
+if(sessionStorage.getItem("item")==null){
+  sessionStorage.setItem("counter",(0));
+  sessionStorage.setItem("totalPrice",(0));
+}
+var counter = Number(sessionStorage.getItem('counter'));
+var totalPrice = Number(sessionStorage.getItem('totalPrice'));
+var deliveryCharge = 0;
+function addMe(n){
+    counter += 1;
+    if (counter>0){
+      deliveryCharge = 30;
+    }
+    var type ="";
+    var path ="";
+    var i = n-1;
+    var cp = document.getElementsByClassName("menuItem");
+    var pr = document.getElementsByClassName("price");
+    var item = cp[i].textContent;
+    var  price = pr[i].textContent;
+    var prc = price.split('.');
+    totalPrice += Number(prc[1]);
+    if(i<10){
+      type = "Veg. Pizza";
+      path = "vegPizza";
+    }
+    else if(i>=10 && i<20){
+      type = "Non-Veg. Pizza";
+      path = "nVegPizza";
+    }
+    else if(i>=20 && i<30){
+      type = "Veg. Burger";
+      path = "vegBurger";
+    }
+    else if(i>=30 && i<40){
+      type = "Non-Veg. Burger";
+      path = "nVegBurger";
+    }
+    else if(i>=40 && i<50){
+      type = "Veg. Frankie";
+      path = "vegFrankie";
+    }
+    else if(i>=50 && i<60){
+      type = "Non-Veg. Frankie";
+      path = "nVegFrankie";
+    }
+    else if(i>=60 && i<70){
+      type = "Veg. Pasta";
+      path = "vegPasta";
+    }
+    else if(i>=70 && i<80){
+      type = "Non-Veg. Pizza";
+      path = "nVegPasta";
+    }
+  document.getElementById("cartItemName").innerHTML = "Add '"+item+"' to cart?";
   document.getElementById("popup_box").style.opacity = "1";
   document.getElementById("popup_box").style.pointerEvents = "auto";
 
   document.getElementById("btn1").addEventListener('click',()=>{
-    document.getElementById("popup_box").style.opacity = "0";
-    document.getElementById("popup_box").style.pointerEvents = "none";
+  document.getElementById("popup_box").style.opacity = "0";
+  document.getElementById("popup_box").style.pointerEvents = "none";
   });
 
   document.getElementById("btn2").addEventListener('click',()=>{
-    document.getElementById("popup_box").style.opacity = "0";
-    document.getElementById("popup_box").style.pointerEvents = "none";
+  document.getElementById("popup_box").style.opacity = "0";
+  document.getElementById("popup_box").style.pointerEvents = "none";
 
-  var item1 = '<div class="listItem col-xs-10">'+e.target.innerHTML+'</div>';
-  var item2 = '<div class="imgItem col-xs-2"><img src="Assets\\'+s+'.jpg" width="100%" height="100%"></div>';
+  var divMenuItem = '<div class = "divItemCartFrame container"><div class = "divItemCart">'+
+    '<div class = "col-xs-9"><div class="cartItemName">'+item+'</div>'+
+    '<div class="cartItemType">'+type+'</div><div class="cartItemPrice">'+price+'</div></div>'+
+    '<div class = "col-xs-3"><div><img src="Assets/'+path+'.jpg" width="80%" height="80%"></div>'+
+    '<div class="cartItemQty">Qty: 1 <span class="glyphicon glyphicon-menu-down"></span></div></div></div>'+
+    '<div id = "cartTilesDiv"><div class="cartTiles col-xs-6" onclick="removeItem()"><span class="glyphicon glyphicon-gift"></span> Save for later</div>'+
+    '<div class="cartTiles col-xs-6"><span class="glyphicon glyphicon-trash"></span> Remove</div></div></div>';
+
+  var priceDetail = '<h3 id="priceDetailHead">Price Details</h3>'+
+    '<div class="divPriceText col-xs-6">Price ('+counter+' item)</div>'+
+    '<div class="divPriceAmount col-xs-6">Rs. '+totalPrice+'</div>'+
+    '<div class="divPriceText col-xs-6">Delivery Charges</div>'+
+    '<div class="divPriceAmount col-xs-6">Rs. '+deliveryCharge+'</div>'+
+    '<hr id="hrPrice" class="col-xs-12">'+
+    '<div class="divPriceText col-xs-6">Total Amount</div>'+
+    '<div class="divPriceAmount col-xs-6">Rs. '+(totalPrice+deliveryCharge)+'</div>';
+
+  var totalPriceDetail = '<div id="totalAmount" class="col-xs-6">'+(totalPrice+deliveryCharge)+'<br>'+
+    '<a href="#priceDetail" id="veiwDetails">Veiw price details</a></div>'+
+    '<div class="col-xs-6"><button id="placeOrder" value="Place Order">'+
+    'Place Order</button></div>';
+
   if(sessionStorage.getItem("item")!=null){
-      var t = sessionStorage.getItem("item");
-      sessionStorage.setItem("item",(t+item1+item2));
+    var t = sessionStorage.getItem("item");
+    sessionStorage.setItem("item",(t+divMenuItem));
   }
   else{
-      sessionStorage.setItem("item",(item1+item2));
+    sessionStorage.setItem("item",(divMenuItem));
   }
-
+  sessionStorage.setItem("priceDetail",(priceDetail));
+  sessionStorage.setItem("totalPriceDetail",(totalPriceDetail));
+  sessionStorage.setItem("counter",(counter));
+  sessionStorage.setItem("totalPrice",(totalPrice));
   });
   
-}
-
-for(var i =0; i<10; i++){
-  vp[i].addEventListener("click",(e)=>{
-      addMe("vegPizza",e);
-  });
-  nvp[i].addEventListener("click",(e)=>{
-    
-  });
-  vb[i].addEventListener("click",(e)=>{
-    
-  });
-  nvb[i].addEventListener("click",(e)=>{
-    
-  });
-  vf[i].addEventListener("click",(e)=>{
-    
-  });
-  nvf[i].addEventListener("click",(e)=>{
-    
-  });
-  vpa[i].addEventListener("click",(e)=>{
-    
-  });
-  nvpa[i].addEventListener("click",(e)=>{
-    
-  });
 }
